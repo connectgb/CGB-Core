fa729506e9005ba3cd27acccffdcd4b9d3b17201
Isaac Diaby <32682957+isaac-diaby@users.noreply.github.com>
2019-05-27 00:18:44 +0100

    GameConfirmationStage Function
    - added the validation helper.
    - added uuidv4  function for game ID feild.
    - made the message listen to only players that are part of the game reactions.
    - made a custume responce to accepted match, rejected match and ignored matches.
    -BUG - need to make the whole function stop one an error has been met in the validation section of the helper function! 
    - ready to add function GameLifeCycle!

--- src/GameCommands/Connect4/index.ts
+++ src/GameCommands/Connect4/index.ts
@@ -1,13 +1,28 @@
-import { OnlineGames } from '../OnlineGame';
-
 import * as Discord from 'discord.js';
+import mongoose from 'mongoose';
+import { OnlineGames } from '../OnlineGame';
+import { IGameMetaInfo } from '../../Models/gameState';
 
 export default class Connect4 extends OnlineGames {
-	constructor(
-		client: Discord.Client,
-		message: Discord.Message,
-		cmdArguments: Array<string>
-	) {
-		super(client, message, cmdArguments);
-	}
+  metaConfig: IGameMetaInfo = {
+    title: 'Connect 4',
+    numPlayers: 2,
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5R4_FiAZMoc0RAFLMSLPt7_IocF6WC0SM7t7yWaxGDyAhY7x5mg',
+  };
+  constructor(
+    client: Discord.Client,
+    message: Discord.Message,
+    cmdArguments: Array<string>
+  ) {
+    super(client, message, cmdArguments);
+    // const cUser = message.guild.member(
+    //   message.mentions.users.first() || message.guild.members.get(this.args[0])
+    // );
+    this.GameConfirmationStage(this.metaConfig).then(start => {
+      start ? console.log('running next') : console.log('STOP');
+
+      // console.log(this.gameMetaData);
+    });
+  }
 }
--- src/GameCommands/index.ts
+++ src/GameCommands/index.ts
@@ -1,4 +1,5 @@
-import Connect4 from './Connect4'
-export const GameCommandsOBJ: { [key:string]:any; } = {
-  'connect4': Connect4
-}
+import Connect4 from './Connect4';
+export const GameCommandsOBJ: { [key: string]: any } = {
+  connect4: Connect4,
+  '!leaveGame': null, //TODO a class that will make you leave the game and make the opponent automaticly win!
+};
