ff8593ada5c412305c73993630ed4a943a532143
Isaac Diaby <32682957+isaac-diaby@users.noreply.github.com>
2019-05-26 12:02:33 +0100

    v0.1
    - can now add the user into the db when they run their first command in the guild.
    - build a function to easily find users buy userID and GuildID

--- src/GameCommands/Connect4/index.ts
+++ src/GameCommands/Connect4/index.ts
@@ -1,13 +1,13 @@
+import * as Discord from 'discord.js';
+import mongoose from 'mongoose';
 import { OnlineGames } from '../OnlineGame';
 
-import * as Discord from 'discord.js';
-
 export default class Connect4 extends OnlineGames {
-	constructor(
-		client: Discord.Client,
-		message: Discord.Message,
-		cmdArguments: Array<string>
-	) {
-		super(client, message, cmdArguments);
-	}
+  constructor(
+    client: Discord.Client,
+    message: Discord.Message,
+    cmdArguments: Array<string>
+  ) {
+    super(client, message, cmdArguments);
+  }
 }
