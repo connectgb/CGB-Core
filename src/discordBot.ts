285380ef754a8b21b8bb6e245eb09823716a6405
Isaac Diaby <32682957+isaac-diaby@users.noreply.github.com>
2019-07-10 00:21:31 +0100

    ADDED: DBL.js
    - DBL 'server count update' is up and running
    - the bot can no longer respond to another bot message.
    - removed the error that users would get is 'no command found' when a user ran the prefix
    - cleaned up the webserver a bit

--- src/discordBot.ts
+++ src/discordBot.ts
@@ -1,39 +1,77 @@
 import * as Discord from 'discord.js';
+import DBLAPI from 'dblapi.js';
 import { GameCommandsOBJ } from './Commands';
 import { UserMD, IUserState } from './Models/userState';
+
 
 export class DiscordBotRun {
   mainGuildData = {
     id: '566982444822036500',
+
     channels: {
-    patronReward: {
-      channel: '588462707594887185',
-      message: '589435499496603648',
-        }   
-    }
+      patronReward: {
+        channel: '588462707594887185',
+        message: '589435499496603648',
+      },
+    },
   };
+
+  port: number = parseInt(process.env.PORT, 10) || 5000;
   botClient: Discord.Client;
+  dbl: DBLAPI;
 
   constructor() {
     this.botClient = new Discord.Client();
     this.botClient.login(process.env.BOT_AUTHTOKEN);
+    if (process.env.PRODUCTION === 'True') {
+      this.dbl = new DBLAPI(
+        process.env.DBL_WEBHOOK_TOKEN,
+        {
+          webhookAuth: process.env.DBL_WEBHOOK_SECRET,
+          webhookPort: this.port,
+        },
+        this.botClient
+      );
+    }
 
     this.botClient.on('ready', () => {
       this.botClient.user.setActivity('Discord Mini Games | ~help ');
       console.log(`${this.botClient.user.username} is online`);
+      if (process.env.PRODUCTION === 'True') {
+        this.dblOnlineListen();
+        //@ts-ignore
+        // this.dbl.webhook.on('ready', hook => {
+        //   console.log(
+        //     `Webhook running: http://${hook.hostname}:${hook.port}${hook.path}`
+        //   );
+        // });
+      }
     });
 
-    this.BotOnlineListen();
+    this.botOnlineListen();
   }
 
-  BotOnlineListen() {
+  dblOnlineListen() {
+    this.dbl.on('posted', () => {
+      console.log('Server count posted!');
+    });
+    this.dbl.on('error', (e: any) => {
+      console.log(`Oops! ${e}`);
+    });
+  }
+
+  botOnlineListen() {
     this.botClient.on('message', receivedMessage => {
       // console.log(receivedMessage);
+
       if (!receivedMessage.content.startsWith(process.env.BOT_PREFIX)) {
         return;
       }
-      // Prevent bot from responding to its own messages
-      if (receivedMessage.author === this.botClient.user) {
+      // Prevent bot from responding to its own messages and other bots
+      if (
+        receivedMessage.author === this.botClient.user ||
+        receivedMessage.author.bot
+      ) {
         return;
       }
       // check if users info is in the DB else create it
@@ -47,7 +85,6 @@ export class DiscordBotRun {
               receivedMessage.channel,
               receivedMessage.guild.id
             );
-
             return true;
           } else if (
             userData.serverAccounts.get(receivedMessage.guild.id) === undefined
@@ -65,6 +102,7 @@ export class DiscordBotRun {
               .split(' ');
             let primaryCmd = commands[0];
             let argsCmd = commands.slice(1);
+
             switch (userData.ingame.isInGame && primaryCmd !== '!leave') {
               case true:
                 const youAreAlreadyInAGameMSG = new Discord.RichEmbed()
@@ -73,13 +111,15 @@ export class DiscordBotRun {
                   .setDescription(
                     `You are already in a game, you cant run any other commands untill your current game is over`
                   )
-                  .addField('solution:', `Run ~!leave game (to for-fit the game)`);
+                  .addField(
+                    'solution:',
+                    `Run ~!leave game (to for-fit the game)`
+                  );
                 receivedMessage.channel.send(youAreAlreadyInAGameMSG);
                 break;
               default:
                 // parsing the command sent to the bot to main command and arguments
                 let gameCommandClass = GameCommandsOBJ[primaryCmd];
-
                 if (!gameCommandClass) {
                   this.noCommandsFound(receivedMessage, primaryCmd);
                 } else if (
@@ -95,7 +135,6 @@ export class DiscordBotRun {
                   );
                 }
                 //receivedMessage.delete();
-
                 break;
             }
           }
@@ -109,8 +148,9 @@ export class DiscordBotRun {
       .setColor('#F44336')
       .setDescription(`${Msg.author}`)
       .addField('Error:', `The command "${triedCmd}" does not exist!`);
-    Msg.channel.send(primaryCmdErrorMSG);
+    // Msg.channel.send(primaryCmdErrorMSG);
   }
+
   async createNewAccount(
     userData: IUserState,
     userDiscordInfo: Discord.User,
@@ -123,7 +163,8 @@ export class DiscordBotRun {
     if (
       (userData._sub.ConnectedLevel > 0 &&
         userData._sub.accountsLimit > userData.serverAccounts.size) ||
-      userData.serverAccounts.size === 0 || guildID == this.mainGuildData.id
+      userData.serverAccounts.size === 0 ||
+      guildID === this.mainGuildData.id
     ) {
       UserMD.findOneAndUpdate(
         { userID: userDiscordInfo.id },
@@ -142,10 +183,14 @@ export class DiscordBotRun {
             .setDescription(
               'You are now part of the system for this server. Now you have access to play games on this server! Have fun winning!'
             )
-            .addField('Join The Official Server', 'http://bit.ly/CGBofficialServer')
+            .addField(
+              'Join The Official Server',
+
+              'http://bit.ly/CGBofficialServer'
+            )
             .setFooter(
-            'For more features and exclusive bonuses become a Donater!: http://bit.ly/CGBdonate'
-          );
+              'For more features and exclusive bonuses become a Donater!: http://bit.ly/CGBdonate'
+            );
           discordChannel.send(newAccountMember);
         });
     } else {
@@ -158,18 +203,21 @@ export class DiscordBotRun {
         )
         .addField(
           'Upgrade Your Connection level (recommended)',
+
           'Make sure you are in the official server ( http://bit.ly/CGBofficialServer ). Increase your connection level by donating towords a/next tier @ http://bit.ly/CGBdonate then use the ~claim perks command in the official server to activate your new perks!'
         )
         .addField(
           'Delete unused existing account on other servers',
+
           '~!delete account - This will delete the account that the current server is using. This will allow you to free up some of your free account slots for the servers you really want an account on.'
         )
         .setFooter(
-            'For more features and exclusive bonuses become a Donater!: http://bit.ly/CGBdonate'
-          );
+          'For more features and exclusive bonuses become a Donater!: http://bit.ly/CGBdonate'
+        );
       discordChannel.send(newAccountmemberFailedNotAPrtron);
     }
   }
+
   async createNewUserProfile(
     userDiscordInfo: Discord.User,
     discordChannel:
@@ -196,9 +244,12 @@ export class DiscordBotRun {
           .setDescription(
             'I see that this is your first time using the Connect Games Bot (CGB). Go to our website to learn what it can do or type ~help!'
           )
-//          .addField('Website', '') TODO: add website link
+          //          .addField('Website', '') TODO: add website link
           .addField('discordbots.org', 'http://bit.ly/CGBdiscordBots')
-          .addField('Join The Official Serverr', 'http://bit.ly/CGBofficialServer')
+          .addField(
+            'Join The Official Serverr',
+            'http://bit.ly/CGBofficialServer'
+          )
           .setFooter(
             'For more features and exclusive bonuses become a Donater!: http://bit.ly/CGBdonate'
           );
