77a4a56692a3f9ef1b084501d4aba9963fb3a0bd
Isaac Diaby <32682957+isaac-diaby@users.noreply.github.com>
2019-07-10 00:19:06 +0100

    Update server.ts
    cleaned up a bit

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,14 @@
 import express from 'express';
-export class Server {
-  app = express();
-  port = 8080; // default port to listen
 
-  // define a route handler for the default home page
+const app = express();
+const port = process.env.PORT || 8080; // default port to listen
 
-  constructor() {
-    this.app.get('/', (req, res) => {
-      res.send('Connect Games Website! Yep this is the right place!');
-    });
+// define a route handler for the default home page
+app.get('/', (req, res) => {
+  res.send('Connect Games Website! Yep this is the right place!');
+});
+
 //     start the Express server
-    this.app.listen(this.port, () => {
-      console.log(`server started at http://localhost:${this.port}`);
-    });
-  }
-}
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
